8d97466d0b74e897aec92af6e92fd2b959c88273
Gustavo Orth <gustavo.241004@bento.local>
2022-05-11 09:59:19 -0300

    supimpa

--- backend/excluir.php
+++ backend/excluir.php
@@ -4,20 +4,11 @@ $id = $_GET['id'];
 $banco = new Banco;
 $conn = $banco->conectar();
 
-
 // dependendo do valor que vier em registro, nós inserimos em uma tabela diferente
 // 1 = ingrediente
 // 2 = item
 // 3 = usuario
 // 4 = cardapio
-
-function findByPk($id){
-    $db = new PDO("mysql:host=localhost;dbname=pw3", "root", "");
-    $consulta = $db->prepare("SELECT * FROM :tabela WHERE id=:id");
-    $consulta->execute([':id' => $id]);
-    $consulta->setFetchMode(PDO::FETCH_CLASS, 'Pessoa');
-    return $consulta->fetch();
-}
 
 switch ($_GET['tabela']) {
     case 1:
@@ -34,19 +25,26 @@ switch ($_GET['tabela']) {
         break;
 }
 
+function findByPk($id){
+    $db = new PDO("mysql:host=localhost;dbname=refeicoes", "root", "");
+    $consulta = $db->prepare("SELECT * FROM :tabela WHERE id=:id");
+    $consulta->execute([':id' => $id, ':tabela' => $tabela]);
+    return $consulta;
+}
+
 function remover(){
     try {
-        $db = new PDO("mysql:host=localhost;dbname=pw3", "root", "");
-        $consulta = $db->prepare("DELETE FROM pessoas WHERE id= :id");
-        $consulta->execute([':id' => $this->id]);
+        $db = new PDO("mysql:host=localhost;dbname=refeicoes", "root", "");
+        $consulta = $db->prepare("DELETE FROM :tabela WHERE id= :id");
+        $consulta->execute([':id' => $id, ':tabela' => $tabela]);
     }catch(PDOException $e){
         die($e->getMessage());
     }
+}
 
-$p = Pessoa::findByPk($id);
-if (!$p) throw new Exception("Usuário não encontrado!");
-$p->remover();
-print $p;
-
+$item = findByPk($id);
+if (!$item) throw new Exception("item da tabela não encontrado!");
+$item->remover();
+print $item;
 
 ?>
