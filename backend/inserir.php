df0b85421f3cfbdc74010f07d6ad3f98e00ba41a
Alef Pasqualotto <alef.131204@bento.local>
2022-05-17 09:56:39 -0300

    praticamente finalizacao inserir

--- backend/inserir.php
+++ backend/inserir.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+//header("Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 include_once('conecta.php');
 $dados = $_POST;
 $banco = new Banco;
@@ -18,20 +18,33 @@ try{
 
 switch ($dados['registro']) {
     case 1:
-        $query = $conn->prepare('INSERT INTO ingrediente (nome, calorias) VALUES (:nome, :calorias);');        
+        $query = $conn->prepare('INSERT INTO ingrediente (nome, calorias) VALUES (:nome, :calorias);');
         $query->execute([
             ':nome' => $dados['nome'],
             ':calorias' => $dados['calorias']
         ]);
         break;
     case 2:
-        $query = $conn->prepare('INSERT INTO item (nome, calorias) VALUES (:descricao);');        
+        $query = $conn->prepare('INSERT INTO item (descricao) VALUES (:descricao);');
         $query->execute([
-            ':descricao' => $dados['descricao']            
+            ':descricao' => $dados['descricao']
         ]);
+        
+        $query = $conn->prepare("SELECT LAST_INSERT_ID()");
+        $query->execute();
+        $item_id = $query->fetch(PDO::FETCH_NUM);
+
+        foreach(explode(',', $dados['ingredientes']) as $ingrediente){
+            $query = $conn->prepare('INSERT INTO ingrediente_item (ingrediente_id, item_id) VALUES (:ingrediente_id, :item_id);');
+            
+            $query->execute([
+                ':ingrediente_id' => $ingrediente,
+                ':item_id' => $item_id[0]
+            ]);
+        }
         break;
-    case 3:        
-        $query = $conn->prepare('INSERT INTO usuario (nome, senha, email, crn) VALUES (:nome, :senha, :email, :crn);');        
+    case 3:
+        $query = $conn->prepare('INSERT INTO usuario (nome, senha, email, crn) VALUES (:nome, :senha, :email, :crn);');
         $query->execute([
             ':nome' => $dados['nome'],
             ':senha' => $dados['senha'],
@@ -39,14 +52,26 @@ switch ($dados['registro']) {
             ':crn' => isset($dados['senha']) ? $dados['senha'] : null
         ]);
         break;
-    case 4:        
-        $query = $conn->prepare('INSERT INTO cardapio (dt, tipo, nutricionista_id) VALUES (:dt, :tipo, :nutricionista);');        
+    case 4:
+        $query = $conn->prepare('INSERT INTO cardapio (dt, tipo, nutricionista_id) VALUES (:dt, :tipo, :nutricionista);');
         $query->execute([
             ':dt' => $dados['data'],
             ':tipo' => $dados['tipo'],
             ':nutricionista' => $dados['nutricionista']
-
         ]);
+        
+        $query = $conn->prepare("SELECT LAST_INSERT_ID()");
+        $query->execute();
+        $item_id = $query->fetch(PDO::FETCH_NUM);
+
+        foreach(explode(',', $dados['ingredientes']) as $ingrediente){
+            $query = $conn->prepare('INSERT INTO ingrediente_item (ingrediente_id, item_id) VALUES (:ingrediente_id, :item_id);');
+            
+            $query->execute([
+                ':ingrediente_id' => $ingrediente,
+                ':item_id' => $item_id[0]
+            ]);
+        }
         break;
 }
 ?>
