5938242c7df12585ab2027e5ca6b7c053f4cd5a6
Alef Pasqualotto <alef.131204@bento.local>
2022-05-31 09:38:49 -0300

    inserir item funciona

--- frontend/system/add-refeicao.php
+++ frontend/system/add-refeicao.php
@@ -1,8 +1,7 @@
 <?php session_start();
-include_once('includes/config.php');
 include_once(__DIR__ . '..\..\..\backend\conecta.php');
-if(strlen( $_SESSION["usuario_id"])==0)
-{   header('location:logout.php');
+if (strlen($_SESSION["usuario_id"]) == 0) {
+    header('location:logout.php');
 }
 ?>
 
@@ -11,84 +10,85 @@ if(strlen( $_SESSION["usuario_id"])==0)
 
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Adicionar Refeição</title>
-       <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-  
 
-    </head>
-    <body>
-   <?php include_once('includes/header.php');?>
-        <div id="layoutSidenav">
-   <?php include_once('includes/leftbar.php');?>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Adicionar Refeição</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="cardapio.php">Cardápio</a></li>
-                            <li class="breadcrumb-item active">Adicionar Refeição</li>
-                        </ol>
-                        <div class="card mb-4">
-                            <div class="card-body">
-<form  method="post" action="../../backend/inserir.php">                                
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Adicionar Refeição</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
 
+</head>
 
-<div class="row" style="margin-top:1%;">
-<div class="col-2">Nome do Prato:</div>
-<div class="col-6"><input type="text" required name="notetitle" placeholder="Insira o nome do prato" class="form-control"></textarea>
-</div>
-</div>
+<body>
+    <?php include_once('includes/header.php'); ?>
+    <div id="layoutSidenav">
+        <?php include_once('includes/leftbar.php'); ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Adicionar Refeição</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item"><a href="cardapio.php">Cardápio</a></li>
+                        <li class="breadcrumb-item active">Adicionar Refeição</li>
+                    </ol>
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <form method="post" action="../../backend/inserir.php">
+                                <div class="row" style="margin-top:1%;">
+                                    <div class="col-2">Nome do Prato:</div>
+                                    <input type="hidden" name="registro" value='2'>
+                                    <div class="col-6"><input type="text" required name="descricao" placeholder="Insira o nome do prato" class="form-control"></textarea>
+                                    </div>
+                                </div>
+<!-- 
+                                <div class="row" style="margin-top:1%;">
+                                    <div class="col-2">Tipo:</div>
+                                    <div class="col-6">
+                                        <select name="tipoprato" required id="tipoprato" class="form-control">
+                                            <option value="" disabled selected hidden>Insira o tipo da refeição</option>
+                                            <option value="cafe">Café</option>
+                                            <option value="almoco">Almoço</option>
+                                            <option value="janta">Janta</option>
+                                        </select>
+                                    </div>
+                                </div> -->
 
-<div class="row" style="margin-top:1%;">
-<div class="col-2">Tipo:</div>
-<div class="col-6">
-<select name="tipoprato" required id="tipoprato" class="form-control">
-  <option value="" disabled selected hidden>Insira o tipo da refeição</option>
-  <option value="cafe">Café</option>
-  <option value="almoco">Almoço</option>
-  <option value="janta">Janta</option>
-</select>
-</div>
-</div>
-
-<div class="row" style="margin-top:1%;">
-<div class="col-2">Ingredientes:</div>
-<div class="col-6">
-<?php include_once('includes/ingredientes.php');?>
-</div>
-</div>
+                                <div class="row" style="margin-top:1%;">
+                                    <div class="col-2">Ingredientes:</div>
+                                    <div class="col-6">
+                                        <?php include_once('includes/ingredientes.php'); ?>
+                                    </div>
+                                </div>
 
 
 
 
-<div class="row" style="margin-top:1%">
-<div class="col-2">&nbsp;</div>
-<div class="col-2"><button type="submit" name="submit" class="btn btn-primary">Enviar</button></div>
-</div>
+                                <div class="row" style="margin-top:1%">
+                                    <div class="col-2">&nbsp;</div>
+                                    <div class="col-2"><button type="submit" name="submit" class="btn btn-primary">Enviar</button></div>
+                                </div>
 
-</form>
-                            </div>
+                            </form>
                         </div>
                     </div>
+                </div>
 
-                </main>       
-     </div>
+            </main>
         </div>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+
 </html>
