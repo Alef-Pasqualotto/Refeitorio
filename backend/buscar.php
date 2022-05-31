c9ace4e1c97ff1696da25b340ac4646469eafc91
Gustavo Orth <gustavo.241004@bento.local>
2022-05-31 10:00:32 -0300

    case 1 querado mas ta ino

--- backend/buscar.php
+++ backend/buscar.php
@@ -14,14 +14,15 @@ $ingredientes = [];
 
 switch ($_POST['tabela']) {
     case 1:
-        $query = $conn->prepare(' SELECT ingrediente.nome, ingrediente.calorias, item.descricao, cardapio.dt, cardapio.tipo, usuario.nome, usuario.crn 
+        $query = $conn->prepare(' SELECT item.descricao as 'nome_do_prato', CONCAT(CASE WHEN ingrediente_item.item_id = ingrediente_item.item_id THEN ingrediente.nome ELSE 0 END) as 'ingredientes', cardapio.dt, cardapio.tipo, usuario.nome, usuario.crn,SUM(CASE WHEN ingrediente_item.item_id = ingrediente_item.item_id THEN calorias ELSE 0 END) as 'soma_das_calorias'
         from cardapio_item INNER JOIN cardapio ON cardapio_item.cardapio_id = cardapio.cardapio_id 
         INNER JOIN item on cardapio_item.item_id = item.item_id INNER JOIN ingrediente_item on item.item_id = ingrediente_item.item_id 
         INNER JOIN ingrediente on ingrediente_item.ingrediente_id = ingrediente.ingrediente_id INNER JOIN usuario on cardapio.nutricionista_id = usuario.usuario_id 
-        WHERE cardapio.tipo = :tipo GROUP BY item.descricao  ;');        
+        WHERE cardapio.tipo = 3 GROUP BY nome_do_prato;');        
         $query->execute([
             ':tipo' => $dados['tipo']
         ]);
+        echo '<pre>';
         var_dump($query->fetch());
         break;
         case 2:
@@ -33,7 +34,7 @@ switch ($_POST['tabela']) {
             echo '<pre>';
             $query->execute([
                 ':dt' => $dados['dt']                          
-            ]); 
+            ]);
             var_dump($query->fetch());
             break;
         case 3:        
