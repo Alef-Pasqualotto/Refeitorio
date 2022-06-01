9ea5f90db121ca20b7731d18b8761a8932583b2b
Giovanni Zini <giovanni.050804@bento.local>
2022-06-01 09:56:31 -0300

    inserir ingrediente

--- frontend/system/includes/leftbar.php
+++ frontend/system/includes/leftbar.php
@@ -16,9 +16,21 @@
                              <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="add-refeicao.php">Adicionar</a>
-                                    <a class="nav-link" href="manage-notes.php">Administrar</a>
+                                    <a class="nav-link" href="manage-refeicao.php">Administrar</a>
                                 </nav>
                             </div>
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Ingredientes
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="add-ingrediente.php">Adicionar</a>
+                                </nav>
+                            </div>
+
                                <a class="nav-link" href="logout.php">
                                 <div class="sb-nav-link-icon"><i class="fa fa-sign-out"></i></div>
                              Logout
