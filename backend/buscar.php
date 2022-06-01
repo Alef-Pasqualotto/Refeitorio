62ed7bf6fbdf0c83c577e000878838833e8c8526
Cesar Torbes <cesar.110205@bento.local>
2022-06-01 09:12:17 -0300

    sla

--- backend/buscar.php
+++ backend/buscar.php
@@ -28,11 +28,13 @@ switch ($_POST['tabela']) {
         var_dump($query->fetch(PDO::FETCH_ASSOC));
         break;
         case 2: //selecionar por data ex:0000-00-00
-            $query = $conn->prepare('SELECT ingrediente.nome, ingrediente.calorias, item.descricao, cardapio.dt, cardapio.tipo, usuario.nome, usuario.crn 
+            $query = $conn->prepare('SELECT item.descricao as nome_do_prato, GROUP_CONCAT(ingrediente.nome) 
+            as ingredientes, cardapio.dt, cardapio.tipo, usuario.nome, usuario.crn,
+            SUM(CASE WHEN ingrediente_item.item_id = ingrediente_item.item_id THEN calorias ELSE 0 END) as soma_das_calorias 
             from cardapio_item INNER JOIN cardapio ON cardapio_item.cardapio_id = cardapio.cardapio_id 
             INNER JOIN item on cardapio_item.item_id = item.item_id INNER JOIN ingrediente_item on item.item_id = ingrediente_item.item_id 
             INNER JOIN ingrediente on ingrediente_item.ingrediente_id = ingrediente.ingrediente_id INNER JOIN usuario on cardapio.nutricionista_id = usuario.usuario_id 
-            WHERE cardapio.dt = :dt GROUP BY item.descricao  ;');        
+            WHERE cardapio.dt = :dt GROUP BY nome_do_prato ;');        
             echo '<pre>';
             $query->execute([
                 ':dt' => $dados['dt']                          
@@ -47,9 +49,12 @@ switch ($_POST['tabela']) {
             INNER JOIN ingrediente on ingrediente_item.ingrediente_id = ingrediente.ingrediente_id
             INNER JOIN usuario on cardapio.nutricionista_id = usuario.usuario_id
             WHERE item.descricao LIKE '%:descricao%' ;");        
+            echo '<pre>';
             $query->execute([
                 ':descricao' => $dados['descricao']
             ]);
             break;
+            var_dump($query->fetch(PDO::FETCH_ASSOC));
+            
     }
 ?>
