df6ab92b053d9509892d3533e1a3b472cc7d59f6
Giovanni Zini <giovanni.050804@bento.local>
2022-06-14 09:40:50 -0300

    verde shippuuden

--- frontend/system/add-refeicao.php
+++ frontend/system/add-refeicao.php
@@ -63,7 +63,7 @@ if (strlen($_SESSION["usuario_id"]) == 0) {
 
                                 <div class="row" style="margin-top:1%">
                                     <div class="col-2">&nbsp;</div>
-                                    <div class="col-2"><button type="submit" name="submit" class="btn btn-primary">Enviar</button></div>
+                                    <div class="col-2"><button type="submit" name="submit" class="btn btn-success">Enviar</button></div>
                                 </div>
 
                             </form>
