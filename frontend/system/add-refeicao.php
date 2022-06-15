f161b00040af8185bb77aa0fb4c748a7a0313479
Alef Pasqualotto <alef.131204@bento.local>
2022-06-15 08:32:06 -0300

    e

--- frontend/system/add-refeicao.php
+++ frontend/system/add-refeicao.php
@@ -59,7 +59,7 @@ if (strlen($_SESSION["usuario_id"]) == 0) {
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-8">
+                                    <div class="col-6">
                                         <?php include_once('includes/ingredientes.php'); ?>
                                     </div>
                                 </div>
@@ -67,7 +67,7 @@ if (strlen($_SESSION["usuario_id"]) == 0) {
 
                                 <div class="row" style="margin-top:1%">
                                     <div class="col-2">&nbsp;</div>
-                                    <div class="col-2"><button type="submit" name="submit" class="btn btn-success">Enviar</button></div>
+                                    <div class="col-2"><button type="submit" name="submit" class="btn btn-primary">Enviar</button></div>
                                 </div>
 
                             </form>
