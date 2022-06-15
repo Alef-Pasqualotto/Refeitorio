9de8a30f59997b5b16c0700c96b9dae82e11887c
Alef-Pasqualotto <pasqualotto.alef@gmail.com>
2022-06-14 21:47:53 -0300

    surpresinha

--- frontend/system/cardapio.php
+++ frontend/system/cardapio.php
@@ -1,4 +1,5 @@
 <?php session_start();
+header('https://www.youtube.com/watch?v=RMMlWvLJBtQ&t=50s');
 error_reporting(0);
 include_once(__DIR__ . '..\..\..\backend\conecta.php');
 if (strlen($_SESSION["usuario_id"]) == 0) {
